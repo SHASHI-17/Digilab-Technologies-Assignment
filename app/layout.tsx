af2932bf97976fbb8a934e4ab5cc29fffe9b65f1
SHASHI-17 <hibye31583@gmail.com>
2024-06-01 00:07:50 +0530

    import fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
 import "./globals.css";
-import useServiceWorker from "./hooks/useServiceWorker";
+
 
 
 const inter = Noto_Sans({
