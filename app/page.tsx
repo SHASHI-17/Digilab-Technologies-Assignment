9cc9d6ece6b64fc760ff89114419129076f308fd
SHASHI-17 <hibye31583@gmail.com>
2024-06-02 04:09:50 +0530

    final design

--- app/page.tsx
+++ app/page.tsx
@@ -1,111 +1,26 @@
 "use client";
-import { useEffect, useState } from "react";
-import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
-import { Vortex } from "@/components/ui/vortex";
+
 import Image from "next/image";
-import addNotification from "react-push-notification";
 
 export default function Home() {
-  const [isBellVisible, setIsBellVisible] = useState(false);
-
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", () => {
-        navigator.serviceWorker
-          .register("/sw.js")
-          .then((registration) => {
-            console.log(
-              "Service Worker registered with scope:",
-              registration.scope
-            );
-          })
-          .catch((error) => {
-            console.error("Service Worker registration failed:", error);
-          });
-      });
-    }
-  }, []);
-
   const notificationPush = (
     notificationText = "Thank You for enabling notifications!"
   ) => {
-    // addNotification({
-    //   title: 'DigiLabs Assignment',
-    //   message: 'Visit my website',
-    //   duration: 7000,
-    //   icon: '/express.png',
-    //   native: true,
-    //   theme: 'darkblue',
-    //   vibrate: 100,
-    // });
-
     if (!("Notification" in window)) {
       alert("Browser does not support notifications");
     } else if (Notification.permission === "granted") {
-      const notification = new Notification(notificationText);
+      new Notification(notificationText);
     } else if (Notification.permission !== "denied") {
       Notification.requestPermission().then((permission) => {
         if (permission === "granted") {
-          const notification = new Notification(notificationText);
+          new Notification(notificationText);
         }
       });
     }
   };
 
-  useEffect(() => {
-    setIsBellVisible(true);
-    const timer = setTimeout(() => {
-      setIsBellVisible(true);
-    }, 100);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
-    // <div className="w-full mx-auto rounded-md h-screen overflow-hidden z-[999]">
-    //   <Vortex
-    //     backgroundColor="black"
-    //     rangeY={800}
-    //     particleCount={1100}
-    //     baseHue={120}
-    //     rangeSpeed={2}
-    //     className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
-    //   >
-    //     <h1 className="text-white text-3xl sm:text-4xl md:text-6xl font-bold text-center">
-    //       DigiLab Technologies Assignment
-    //     </h1>
-    //     <p className="text-white text-sm md:text-2xl max-w-2xl mt-6 text-center">
-    //       This is chemical burn. It&apos;ll hurt more than you&apos;ve ever been
-    //       burned and you&apos;ll have a scar.
-    //     </p>
-    //     {isBellVisible && (
-    //       <div className="mt-[5.5rem] bg-transparent">
-    //         <div className="pulse relative flex items-center justify-center h-36 w-36 bg-transparent rounded-full mx-auto duration-150 transition-all">
-    //           <Image
-    //             src="/bell.svg"
-    //             alt="bell"
-    //             width={100}
-    //             height={100}
-    //             className="z-10 animate-ping"
-    //           />
-    //           <div className="absolute h-full w-full bg-blue-300 rounded-full opacity-70 animate-pulse1"></div>
-    //           <div className="absolute h-full w-full bg-emerald-500 rounded-full opacity-70 animate-pulse2"></div>
-    //         </div>
-    //       </div>
-    //     )}
-    //     <div className="flex flex-col sm:flex-row items-center gap-4 mt-[7.3rem]">
-    //       <HoverBorderGradient
-    //         onClick={()=>notificationPush()}
-    //         containerClassName="rounded-full"
-    //         as="button"
-    //         className="text-white  px-14 py-3 sm:px-10 sm:py-3 flex items-center space-x-2"
-    //       >
-    //         <span className="text-xl sm:text-lg">Send Notification</span>
-    //       </HoverBorderGradient>
-    //     </div>
-    //   </Vortex>
-    // </div>
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white -mt-6">
       <div className="text-center mb-[100px]">
         <h1 className="text-3xl font-semibold mb-2">Lorem Ipsum...</h1>
         <p className="text-gray-400 mb-8">Lorem ipsum dolor sit amet.</p>
@@ -125,10 +40,12 @@ export default function Home() {
           <Image src="/bell.svg" alt="bell" width={50} height={50} />
         </div>
       </div>
-      <button className="mt-[140px] text-[12px] sm:text-[14px] font-bold  bg-gradient-to-r from-[#1e080b] to-[#2b1002] via-[#f5563a] border border-l-[0] border-r-[2px]  border-orange-800 border-r-orange-600 border-solid border-opacity-100 shadow-md shadow-rgba(0, 0, 0, 0.12) gap-[8px] px-[5rem] sm:px-[6rem]  py-2 rounded-lg text-white text-lg">
+      <button
+        onClick={() => notificationPush()}
+        className="mt-[140px] text-[12px] sm:text-[14px] font-bold  bg-gradient-to-r from-[#1e080b] to-[#2b1002] via-[#f5563a] border border-l-[0] border-r-[2px]  border-orange-800 border-r-orange-600 border-solid border-opacity-100 shadow-md shadow-rgba(0, 0, 0, 0.12) gap-[8px] px-[5rem] sm:px-[6rem]  py-2 rounded-lg text-white text-lg"
+      >
         Send Notification
       </button>
-
     </div>
   );
 }
